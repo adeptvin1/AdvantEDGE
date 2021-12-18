8a8dc66e9357c790f05b7d38030181a12ad94403
Ivan Filyanin <adeptvin1@gmail.com>
2021-12-18 10:31:20 +0000

    Add check directory in install_path_nvm

--- playbooks/roles/node/files/install_path_nvm.sh
+++ playbooks/roles/node/files/install_path_nvm.sh
@@ -3,5 +3,9 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-echo "Sucess"
+if [ -d $NVM_DIR ]
+then
+echo "Sucess! NVM dir in $NVM_DIR"
+else
+echo "Error! Selected path is not a correct directory";
+fi
