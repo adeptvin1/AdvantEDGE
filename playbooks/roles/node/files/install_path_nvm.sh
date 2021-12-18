5a2808c0a8abfc88144f865dc24409c71afb592b
Ivan Filyanin <adeptvin1@gmail.com>
2021-12-18 11:01:25 +0000

    Fix

--- playbooks/roles/node/files/install_path_nvm.sh
+++ playbooks/roles/node/files/install_path_nvm.sh
@@ -3,7 +3,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-ln -s $NVM_DIR ~/nvm
 if [ -d $NVM_DIR ]
 then
 echo "Sucess! NVM dir in $NVM_DIR"
