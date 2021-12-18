552ab4d7c995325749763fe7643c61fe958034e5
Ivan Filyanin <adeptvin1@gmail.com>
2021-12-18 10:51:03 +0000

    Add simlink

--- playbooks/roles/node/files/install_path_nvm.sh
+++ playbooks/roles/node/files/install_path_nvm.sh
@@ -3,9 +3,10 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+ln -s $NVM_DIR ~/nvm
 if [ -d $NVM_DIR ]
 then
 echo "Sucess! NVM dir in $NVM_DIR"
 else
-echo "Error! Selected path is not a correct directory";
+echo "Error! Selected path is not a correct directory"; exit 2
 fi
